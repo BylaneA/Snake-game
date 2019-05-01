40c55f17a941235dc7b97a35cae718dde4e51a7a
BylaneA <billieabdiw@gmail.com>
2019-05-02 01:42:18 +0200

    refactor + drawing

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.onload = function()
 {
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    document.body.appendChild(canvas);
+    function init()
+    {
+        var canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+
+        var context = canvas.getContext('2d');
+        context.fillStyle = "red";
+        context.fillRect(30, 30, 100, 50);
+    }
+    
 }
+    
