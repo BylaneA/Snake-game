f904fd3e5069c858f97e4f0f5d0e9eaf305afba2
BylaneA <billieabdiw@gmail.com>
2019-05-13 21:38:18 +0200

    canvas + snake not moving tho

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 window.onload = function()
 {
+    var canvas;
+    var context;
+    var delay = 1000; 
+    var xCoord = 0;
+    var yCoord = 0;
+
+    init();
+
     function init()
     {
-        var canvas = document.createElement('canvas');
+        canvas = document.createElement('canvas');
         canvas.width = 900;
         canvas.height = 600;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
-
-        var context = canvas.getContext('2d');
-        context.fillStyle = "red";
-        context.fillRect(30, 30, 100, 50);
+        context = canvas.getContext('2d');
+        refreshCanvas();
+        
     }
-    
+
+    function refreshCanvas()
+    {
+        xCoord += 2;
+        yCoord +=2;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "red";
+        context.fillRect(xCoord, yCoord, 100, 50);
+    }
+
 }
     
